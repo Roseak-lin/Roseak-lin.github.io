24623424e1939199bd38091d6505e7cea5a7d0e3
Roseak Lin <roseak.lin@gmail.com>
2025-09-06 01:29:59 -0400

    added pagination to photography page

--- src/Pages/Photography/PhotographyPage.tsx
+++ src/Pages/Photography/PhotographyPage.tsx
@@ -1,21 +1,26 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import CustomNavbar from "../../Components/NavBar/Navbar";
-import { Container } from "react-bootstrap";
+import { Button, Container, Row } from "react-bootstrap";
 import "./PhotographyPage.css";
 import CanvasImage from "../../Components/CanvasImage/Image";
 import ScrollToTop from "../../Components/ScrollToTop/ScrollToTop";
 
 const PhotographyPage = () => {
   const [images, setImages] = useState<string[]>([]);
+  const [cursor, setCursor] = useState<string | null | undefined>();
+  const [loading, setLoading] = useState(false);
 
-const WORKER =
-  window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
-    ? "http://127.0.0.1:8787"
-    : "https://personal-website-worker.roseak-lin.workers.dev";
-  useEffect(() => {
-    const fetchImages = async () => {
+  const WORKER =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
+      ? "http://127.0.0.1:8787"
+      : "https://personal-website-worker.roseak-lin.workers.dev";
+
+  const fetchImages = useCallback(
+    async (cursor: string | null | undefined) => {
       try {
-        const response = await fetch(`${WORKER}/getItems`, {
+        setLoading(true);
+        const response = await fetch(`${WORKER}/getItems${cursor ? `?cursor=${cursor}` : ""}`, {
           method: "GET",
         });
 
@@ -34,14 +39,22 @@ const WORKER =
           setImages([]);
           return;
         }
-        setImages(data.images);
+        setCursor(data.cursor);
+        setImages((currImages) => [...currImages, ...data.images]);
       } catch (err) {
         console.error("Network or server error:", err);
         setImages([]);
       }
-    };
+      finally {
+        console.log("Finished loading");
+        setLoading(false);
+      }
+    },
+    [WORKER]
+  );
 
-    fetchImages();
+  useEffect(() => {
+    fetchImages(undefined);
   }, []);
 
   const imageGrid = () => {
@@ -59,6 +72,12 @@ const WORKER =
     });
   };
 
+  const loadNextImagePage = async () => {
+    if (cursor) {
+      await fetchImages(cursor);
+    }
+  };
+
   return (
     <div>
       <CustomNavbar />
@@ -68,6 +87,10 @@ const WORKER =
           A little collection of my photography :)
         </h1>
         <div className="canvas">{imageGrid()}</div>
+          {/* {cursor && <Button onClick={loadNextImagePage}>Load More</Button>} */}
+        <Row className="m-0 justify-content-center">
+          <Button variant="outline-dark" className="w-75 m-2" onClick={loadNextImagePage} disabled={loading}>{loading ? "Loading..." : "Load More"}</Button>
+        </Row>
       </Container>
     </div>
   );
